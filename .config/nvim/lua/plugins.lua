0320752fe8629caf366cc76e1b8c96756a221df3
jltwheeler <jltwheeler@gmail.com>
2021-08-31 18:53:17 +0100

    move disable default plugins to plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,8 +1,35 @@
+-- bootstrap packer
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd 'packadd packer.nvim'
+end
+
+-- disable some builtin vim plugins
+local disabled_built_ins = {
+   ---2html_plugin---,
+   ---getscript---,
+   ---getscriptPlugin---,
+   ---gzip---,
+   ---logipat---,
+   ---netrw---,
+   ---netrwPlugin---,
+   ---netrwSettings---,
+   ---netrwFileHandlers---,
+   ---matchit---,
+   ---tar---,
+   ---tarPlugin---,
+   ---rrhelper---,
+   ---spellfile_plugin---,
+   ---vimball---,
+   ---vimballPlugin---,
+   ---zip---,
+   ---zipPlugin---,
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+   g["loaded_" .. plugin] = 1
 end
 
 return require('packer').startup(function()
--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -3,10 +3,10 @@ local wo = vim.wo
 local bo = vim.bo
 local g = vim.g
 
---- For spell checking
+-- For spell checking
 opt.spelllang = "en"
 
---- Indentation and tabbing
+-- Indentation and tabbing
 opt.tabstop = 2
 opt.shiftwidth = 2
 opt.smartindent = true
@@ -14,41 +14,41 @@ opt.smarttab = true
 opt.expandtab = true
 opt.cindent = true
 
---- Turn off word wrapping
+-- Turn off word wrapping
 wo.wrap = false
 
---- Disable continuation of comments on next line
+-- Disable continuation of comments on next line
 bo.formatoptions = bo.formatoptions:gsub('cro', '')
 
---- Let language syntax take care of folding blocks
+-- Let language syntax take care of folding blocks
 wo.foldmethod = "syntax"
-opt.foldlevelstart = 20 --- opens folds by default on buffer load
+opt.foldlevelstart = 20 -- opens folds by default on buffer load
 
---- Start vertical scroll when 8 characters away
+-- Start vertical scroll when 8 characters away
 wo.scrolloff = 8
 
---- Natural window splitting feeling
+-- Natural window splitting feeling
 opt.splitbelow = true
 opt.splitright = true
 
---- Searching
+-- Searching
 opt.incsearch = true
 opt.hlsearch = false
 
---- Turn off case sensitive searching
+-- Turn off case sensitive searching
 opt.ignorecase = true
 opt.smartcase = true
 
---- Relative numbers
+-- Relative numbers
 wo.relativenumber = true
 wo.number = true
 
---- Ruler at 80 characters
+-- Ruler at 80 characters
 opt.colorcolumn = "80"
 opt.signcolumn = "yes"
 vim.highlight.create('ColorColumn', {ctermbg=0, guibg=lightgrey}, false)
 
---- Get rid of swap files and backups - generally never use these features.
+-- Get rid of swap files and backups - generally never use these features.
 opt.swapfile = false
 opt.backup = false
 opt.undodir = "~/.vim/undodir"
@@ -57,39 +57,13 @@ opt.undofile = true
 opt.hidden = true
 opt.errorbells = false
 
---- Give more space for displaying messages
+-- Give more space for displaying messages
 opt.cmdheight = 2
 
---- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
---- delays and poor user experience.
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience.
 opt.updatetime = 100
 
---- Don't pass messages to |ins-completion-menu|.
+-- Don't pass messages to |ins-completion-menu|.
 vim.o.shortmess = vim.o.shortmess .. "c"
-
--- disable some builtin vim plugins
-local disabled_built_ins = {
-   ---2html_plugin---,
-   ---getscript---,
-   ---getscriptPlugin---,
-   ---gzip---,
-   ---logipat---,
-   ---netrw---,
-   ---netrwPlugin---,
-   ---netrwSettings---,
-   ---netrwFileHandlers---,
-   ---matchit---,
-   ---tar---,
-   ---tarPlugin---,
-   ---rrhelper---,
-   ---spellfile_plugin---,
-   ---vimball---,
-   ---vimballPlugin---,
-   ---zip---,
-   ---zipPlugin---,
-}
-
-for _, plugin in pairs(disabled_built_ins) do
-   g["loaded_" .. plugin] = 1
-end
 
