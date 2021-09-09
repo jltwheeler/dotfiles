9664b599d4aa4023199d64548c6582b6fb7d9110
jltwheeler <jltwheeler@gmail.com>
2021-09-09 19:25:03 +0100

    add mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -15,6 +15,10 @@ keymap('n', 'sv', ':vsplit<Return><C-w>w', { noremap = false, silent = false })
 keymap('n', '<Tab>', ':bnext<CR>', { noremap = true, silent = false })
 keymap('n', '<S-Tab>', ':bprevious<CR>', { noremap = true, silent = false })
 
+-- Easier indenting
+keymap('v', '<', '<gv', opts)  -- reselect after indent left
+keymap('v', '>', '>gv', opts)  -- reselect after indent right
+
 -- Git diff split view
 keymap('n', '<leader>gs', ':Gdiffsplit<CR>', { noremap = true, silent = false })
 
@@ -25,15 +29,15 @@ keymap('n', '<leader>sc', 'spell!<CR>', { noremap = true, silent = true })
 keymap('n', 'Y', 'y$', { noremap = true, silent = false })
 
 -- Keep searches and joins centered 
-keymap('n', 'n', 'nzzzv', { noremap = true, silent = false })
-keymap('n', 'N', 'Nzzzv', { noremap = true, silent = false })
-keymap('n', 'J', 'mzJ`z', { noremap = true, silent = false })
+keymap('n', 'n', 'nzzzv', { noremap = true, silent = true })
+keymap('n', 'N', 'Nzzzv', { noremap = true, silent = true })
+keymap('n', 'J', 'mzJ`z', { noremap = true, silent = true })
 
 -- Add undo break points for special characters
-keymap('i', ',', ',<c-g>u', { noremap = true, silent = false })
-keymap('i', '.', '.<c-g>u', { noremap = true, silent = false })
-keymap('i', '!', '!<c-g>u', { noremap = true, silent = false })
-keymap('i', '?', '?<c-g>u', { noremap = true, silent = false })
+keymap('i', ',', ',<c-g>u', { noremap = true, silent = true })
+keymap('i', '.', '.<c-g>u', { noremap = true, silent = true })
+keymap('i', '!', '!<c-g>u', { noremap = true, silent = true })
+keymap('i', '?', '?<c-g>u', { noremap = true, silent = true })
 
 -- File explorer
 keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = false })
@@ -41,12 +45,12 @@ keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', { noremap = true, silent = fals
 
 -- Add Telescope leader shortcuts
 keymap('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>]], { noremap = true, silent = true })
-keymap('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', { noremap = true, silent = false })
-keymap('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').git_files()<cr>', { noremap = true, silent = false })
-keymap('n', '<leader>fs', '<cmd>lua require(\'telescope.builtin\').git_status()<cr>', { noremap = true, silent = false })
-keymap('n', '<leader>fw', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', { noremap = true, silent = false })
-keymap('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', { noremap = true, silent = false })
-keymap('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', { noremap = true, silent = false })
+keymap('n', '<leader>ff', '<cmd>lua require(\'telescope.builtin\').find_files()<cr>', { noremap = true, silent = true })
+keymap('n', '<leader>fg', '<cmd>lua require(\'telescope.builtin\').git_files()<cr>', { noremap = true, silent = true })
+keymap('n', '<leader>fs', '<cmd>lua require(\'telescope.builtin\').git_status()<cr>', { noremap = true, silent = true })
+keymap('n', '<leader>fw', '<cmd>lua require(\'telescope.builtin\').live_grep()<cr>', { noremap = true, silent = true })
+keymap('n', '<leader>fb', '<cmd>lua require(\'telescope.builtin\').buffers()<cr>', { noremap = true, silent = true })
+keymap('n', '<leader>fh', '<cmd>lua require(\'telescope.builtin\').help_tags()<cr>', { noremap = true, silent = true })
 keymap('n', '<leader>fd', [[<cmd>lua require('telescope.builtin').grep_string()<cr>]], { noremap = true, silent = true })
 keymap('n', '<leader>fo', [[<cmd>lua require('telescope.builtin').oldfiles()<cr>]], { noremap = true, silent = true })
 keymap('n', '<leader>fv', [[<cmd>lua require('jw.telescope').search_config_nvim()<cr>]], { noremap = true, silent = true })
