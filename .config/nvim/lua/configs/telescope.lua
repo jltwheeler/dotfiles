b2406c5b180907c49d18db0dd2d22a85074fe8cf
Josh Wheeler <jltwheeler@gmail.com>
2022-04-09 21:48:43 +0100

    update telescope config

--- .config/nvim/lua/configs/telescope.lua
+++ .config/nvim/lua/configs/telescope.lua
@@ -10,7 +10,30 @@ local actions = require "telescope.actions"
 telescope.setup {
   defaults = {
     prompt_prefix = " ",
-    selection_caret = " ",
+    selection_caret = "❯ ",
+    entry_prefix = "  ",
+    initial_mode = "insert",
+    selection_strategy = "reset",
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+       horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+          results_width = 0.8,
+       },
+       vertical = {
+          mirror = false,
+       },
+       width = 0.87,
+       height = 0.80,
+       preview_cutoff = 120,
+    },
+    path_display = { "truncate" },
+    winblend = 20,
+    color_devicons = true,
+    use_less = true,
+    set_env = { ["COLORTERM"] = "truecolor" },
     mappings = {
       i = {
         ["<C-n>"] = actions.cycle_history_next,
