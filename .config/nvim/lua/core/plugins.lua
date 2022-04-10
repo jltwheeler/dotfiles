57665de77943f3904d29736ed075847d4e141d97
Josh Wheeler <jltwheeler@gmail.com>
2022-04-10 21:13:23 +0100

    add plugin for faster filetype.vim

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -43,7 +43,9 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used by lots of plugins
-  use 'lewis6991/impatient.nvim'
+  use 'lewis6991/impatient.nvim' -- Faster initial load time of neovim
+
+  use "nathom/filetype.nvim" -- Boost startup time of opening files
 
   -- Color scheme
   use 'folke/tokyonight.nvim'
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -114,6 +114,11 @@ _G.packer_plugins = {
     path = "/Users/jwheeler/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["filetype.nvim"] = {
+    loaded = true,
+    path = "/Users/jwheeler/.local/share/nvim/site/pack/packer/start/filetype.nvim",
+    url = "https://github.com/nathom/filetype.nvim"
+  },
   ["friendly-snippets"] = {
     loaded = true,
     path = "/Users/jwheeler/.local/share/nvim/site/pack/packer/start/friendly-snippets",
